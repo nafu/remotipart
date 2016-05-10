5c96ff8de5efc4ebef42fa8af581e61005a636bb
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-05-11 02:01:23 +0900

    Update engine.rb

--- lib/remotipart/rails/engine.rb
+++ lib/remotipart/rails/engine.rb
@@ -14,7 +14,7 @@ module Remotipart
       end
 
       initializer "remotipart.include_middelware" do
-        config.app_middleware.insert_after ActionDispatch::Request.parameter_parsers, Middleware
+        config.app_middleware.insert_after ActionDispatch::Request.parameter_parsers=, Middleware
       end
     end
 
